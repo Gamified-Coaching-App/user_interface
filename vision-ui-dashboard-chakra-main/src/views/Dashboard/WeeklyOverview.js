d196a10e540a0dfe88fc018939c140513e1235fd
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-02-26 13:00:50 +0000

    commit

--- vision-ui-dashboard-chakra-main/src/views/Dashboard/WeeklyOverview.js
+++ vision-ui-dashboard-chakra-main/src/views/Dashboard/WeeklyOverview.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Flex, Text, Button } from '@chakra-ui/react';
 import {
-  ComposedChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
+    ComposedChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LabelList
 } from 'recharts';
 import TrainingDataContext from '../../contexts/TrainingDataContext'; 
 
@@ -20,6 +20,8 @@ const WeeklyOverview = () => {
     dataByDay[day] = { Distance: 0, MaxHeartRate: 0, Elevation: 0 };
   });
 
+  let totalDistance = 0
+
   trainingData.forEach(activity => {
     const date = new Date(activity.timestamp_local * 1000);
     const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
@@ -27,6 +29,7 @@ const WeeklyOverview = () => {
       dataByDay[dayOfWeek].Distance += activity.distance_meters_total / 1000; // Convert meters to km
       dataByDay[dayOfWeek].MaxHeartRate = Math.max(dataByDay[dayOfWeek].MaxHeartRate, activity.max_heart_rate_in_bpm);
       dataByDay[dayOfWeek].Elevation += activity.elevation_gain_meters_total;
+      totalDistance += activity.distance_meters_total / 1000;
     }
   });
 
@@ -38,70 +41,60 @@ const WeeklyOverview = () => {
     Elevation: dataByDay[day].Elevation,
   }));
 
-  const formatYAxis = (tickItem, name) => {
-    switch(name) {
-      case 'Distance':
-        return `${tickItem} km`;
-      case 'MaxHeartRate':
-        return `${tickItem} bpm`;
-      case 'Elevation':
-        return `${tickItem} m`;
-      default:
-        return tickItem;
-    }
-  };
-
   return (
-    <Flex direction='column' pt={{ base: "120px", md: "75px" }}>
-      <Text fontSize='lg' color='#fff' fontWeight='bold' mb="4">
-        Weekly Running Distance Overview
-      </Text>
-      <Button onClick={refreshData} colorScheme="blue" mb="4">Refresh Data</Button>
-      <ResponsiveContainer width="100%" height={400}>
-    <ComposedChart data={chartData}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis 
-        yAxisId="left" 
-        orientation="left" 
-        stroke="#8884d8" 
-        tickFormatter={(tick) => `${tick} km`}
-      />
-      <YAxis 
-        yAxisId="right" 
-        orientation="right" 
-        stroke="#82ca9d" 
-        tickFormatter={(tick) => `${tick} bpm`}
-      />
-      <YAxis 
-        yAxisId="rightAlt" 
-        orientation="right" 
-        stroke="#82ca9d" 
-        tickFormatter={(tick) => `${tick} m`}
-        axisLine={false}
-        tickLine={false}
-      />
-      <Tooltip />
-      <Legend />
-      <Bar yAxisId="left" dataKey="Distance" fill="#8884d8" />
-      <Line 
-        yAxisId="right" 
-        type="monotone" 
-        dataKey="MaxHeartRate" 
-        stroke="#8884d8" 
-        strokeWidth={3} 
-        activeDot={{ r: 8 }} 
-      />
-      <Line 
-        yAxisId="rightAlt" 
-        type="monotone" 
-        dataKey="Elevation" 
-        stroke="#82ca9d" 
-        strokeWidth={3} 
-      />
-    </ComposedChart>
-  </ResponsiveContainer>
-  </Flex>
+  <Flex direction='column' pt={{ base: "120px", md: "75px" }}>
+    <Text fontSize='lg' color='#fff' fontWeight='bold' mb="4">
+    {totalDistance > 0 ? `Good job! You've run ${totalDistance} km this week.` : `You have not yet completed any workouts this week.`}
+    </Text>
+    <ResponsiveContainer width="100%" height={500}>
+        <ComposedChart data={chartData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis 
+                dataKey="name" 
+                tick={{ fill: '#fff' }} // replace 'yourColorHere' with the color you wan
+            />
+            <YAxis 
+                yAxisId="left" 
+                orientation="left" 
+                stroke="#8884d8" 
+                tickFormatter={(tick) => `${tick} km`}
+            />
+            <YAxis 
+                yAxisId="right" 
+                orientation="right" 
+                stroke="#c9759c" 
+                tickFormatter={(tick) => `${tick} bpm`}
+            />
+            <YAxis 
+                yAxisId="rightAlt" 
+                orientation="right" 
+                stroke="#82ca9d" 
+                tickFormatter={(tick) => `${tick} m`}
+                axisLine={false}
+                tickLine={false}
+            />
+            <Tooltip />
+            <Legend />
+            <Bar yAxisId="left" dataKey="Distance" fill="#8884d8" />
+            <Line 
+                yAxisId="right" 
+                type="monotone" 
+                dataKey="MaxHeartRate" 
+                stroke="#c9759c" 
+                strokeWidth={3} 
+                activeDot={{ r: 8 }} 
+            />
+            <Line 
+                yAxisId="rightAlt" 
+                type="monotone" 
+                dataKey="Elevation" 
+                stroke="#82ca9d" 
+                strokeWidth={3} 
+            />
+        </ComposedChart>
+    </ResponsiveContainer>
+    <Button onClick={refreshData} colorScheme="blue" mt="4" alignSelf="center" width="200px">Refresh Data</Button>
+    </Flex>
   );
 };
 
