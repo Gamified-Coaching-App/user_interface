175bb79191332d9be3844122914a937290330e75
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-03-04 12:24:31 +0000

    old

--- vision-ui-dashboard-chakra-main/src/views/Pages/SignIn.js
+++ vision-ui-dashboard-chakra-main/src/views/Pages/SignIn.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Vision UI Free Chakra - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/vision-ui-free-chakra
-* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
-* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-chakra/blob/master LICENSE.md)
-
-* Design and Coded by Simmmple & Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 import React from "react";
 // Chakra imports
 import {
@@ -197,8 +179,7 @@ function SignIn() {
             display='flex'
             flexDirection='column'
             justifyContent='center'
-            alignItems='center'
-            position='absolute'>
+            alignItems='center'>
             <Text
               textAlign='center'
               color='white'
