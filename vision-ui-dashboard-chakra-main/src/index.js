a5966efe33cf1aeca0202cf8c1ffd9365b12286d
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-03-04 12:25:28 +0000

    index.js

--- vision-ui-dashboard-chakra-main/src/index.js
+++ vision-ui-dashboard-chakra-main/src/index.js
@@ -1,24 +1,33 @@
-// index.js
+// React imports
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
+// Import Amplify and configuration
+import { Amplify } from 'aws-amplify';
+import config from './config/aws-exports'; 
+
+// Layouts and contexts
 import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
 import RTLLayout from "layouts/RTL.js";
-
 import { TrainingDataProvider } from './contexts/TrainingDataContext';
 
-ReactDOM.render(
-  <TrainingDataProvider>
-    <HashRouter>
-      <Switch>
-        <Route path={`/auth`} component={AuthLayout} />
-        <Route path={`/admin`} component={AdminLayout} />
-        <Route path={`/rtl`} component={RTLLayout} />
-        <Redirect from={`/`} to='/admin/dashboard' />
-      </Switch>
-    </HashRouter>
-  </TrainingDataProvider>,
-  document.getElementById("root")
+// Configure Amplify with your AWS configuration
+Amplify.configure(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <TrainingDataProvider>
+      <HashRouter>
+        <Switch>
+          <Route path={`/auth`} component={AuthLayout} />
+          <Route path={`/admin`} component={AdminLayout} />
+          <Route path={`/rtl`} component={RTLLayout} />
+          <Redirect from={`/`} to='/admin/dashboard' />
+        </Switch>
+      </HashRouter>
+    </TrainingDataProvider>
+  </React.StrictMode>
 );
